2111a4b50cfef1693f2e067f3fdc6fac926aed2f
Henna Maria <hennamaria2001@gmail.com>
2024-04-02 15:14:06 +0530

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestmonialCard from "@/components/TestmonialCard";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +14,8 @@ export default function Home() {
           <WhyChooseUs />
           <TestmonialCard />
           <UpcomingWebinars />
+          <Instructors />
+          <Footer />
     </main>
   );
 }
