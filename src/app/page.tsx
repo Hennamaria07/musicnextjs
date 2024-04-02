ce4a03301a209db616ede53e4ffb427b17c547a5
Henna Maria <hennamaria2001@gmail.com>
2024-04-02 14:07:11 +0530

    grid and infinite movind card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestmonialCard from "@/components/TestmonialCard";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+    <main className="min-h-screen bg-black/[0.96] antialiased">
           <HeroSection />
           <FeaturedCourses />
           <WhyChooseUs />
+          <TestmonialCard />
     </main>
   );
 }
